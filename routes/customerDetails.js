2ac145bdd62a95ea18a2acb553f66e86f324e18f
Aravind Wickey <aravind261998@gmail.com>
2022-01-07 17:43:47 +0530

    second Commit

--- routes/customerDetails.js
+++ routes/customerDetails.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var customerDetailsModule = require('../module/customerDetailsModule');
+const authorize = require("../module/Authorize");
 
-router.get("/get", customerDetailsModule.getcustomerDetails);
+router.get(
+  "/get",
+  authorize.AuthorizeUser,
+  customerDetailsModule.getcustomerDetails
+);
 router.post("/create", customerDetailsModule.createCustomer);
 router.post("/login", customerDetailsModule.login);
 router.get("/customerStatus", customerDetailsModule.createCustomer);
