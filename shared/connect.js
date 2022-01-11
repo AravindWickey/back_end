ad6ec9b6b1885ea831115b64bb1a03f4b70861ec
AravindWickey <46027399+AravindWickey@users.noreply.github.com>
2022-01-11 06:01:06 +0530

    commit mongoose db

--- shared/connect.js
+++ shared/connect.js
@@ -1,15 +1,11 @@
-const {MongoClient} = require("mongodb");
-
-
-module.exports =  {
-db:{},
-async connect(){
-try{
-    const client = await MongoClient.connect("mongodb+srv://aravindwickey:ptnYdO8esz1RLjAW@clusterone.3quf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"); 
-    this.db = client.db("integra");
-    console.log(this.db)
-}catch(err){
-    console.log(err)
-}
-}
-}
+const mongoose = require("mongoose");
+exports.connect = () => {
+  try {
+    mongoose.connect(
+      "mongodb+srv://aravindwickey:ptnYdO8esz1RLjAW@clusterone.3quf8.mongodb.net/integra?retryWrites=true&w=majority"
+    );
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
